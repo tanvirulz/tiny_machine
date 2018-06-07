04c0dd045eadb8ee8e92d4c4f49aeb7661823354
tanvirulz <tanvirulbd@gmail.com>
2018-06-07 18:49:05 +0800

    experimenting with documentation

--- memory.c
+++ memory.c
@@ -14,10 +14,14 @@ struct Memory * init_memory(){
 void put_byte(struct Memory * M, uint16_t dp, uint8_t byte){
     int bi , i ;
     bi = dp/MEM_BUCKET_SIZE;
+    
+    /* If specific memory bank that contains dp'th byte is not allocated
+    then the memory is allocated first.*/
     if  (M->mbl[bi] == NULL){
         M->mbl[bi] = (uint8_t *) malloc(MEM_BUCKET_SIZE);
     }
-    i = dp - bi * MEM_BUCKET_SIZE;
+    
+    i = dp - bi * MEM_BUCKET_SIZE; //compute memory bank offset
     M->mbl[bi][i] = byte;
 }
 
--- memory.h
+++ memory.h
@@ -9,6 +9,16 @@ struct Memory{
 };
 
 struct Memory * init_memory();
+
+/**
+ * put_byte() - copy a byte to a specific locaiton in the memory 
+ * @M: pointer to the memory
+ * @dp: memory address
+ * @byte: byte to be stored
+ * 
+ * This function can only wirite one byte at a time to the memory. 
+ * 
+ */
 void put_byte(struct Memory * M, uint16_t dp, uint8_t byte);
 uint8_t get_byte(struct Memory * M, uint16_t dp);
 void free_mem(struct Memory * M);    
